2e80167b7ba3da54bb29eff625c62fc0b083b0dd
Mia <tamiapaschall@gmail.com>
2020-01-14 19:08:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,4 +15,70 @@ class TicTacToe
     ]
     
    def display_board
-     puts "#{@board[0]} | "#{@board[]"}
+     puts "#{@board[0]} | #{@board[1]} | #{@board[2]} "
+     puts "___________"
+     puts "#{@board[3]} | #{@board[4]} | #{@board[5]} "
+     puts "___________"
+     puts "#{@board[6]} | #{@board[7]} | #{@board[8]} "
+   end
+   
+   def input_to_index(user_inout)
+     user_input.to_i - 1 
+   end
+   
+   def move(index, player)
+     @board[index] = player
+   end
+   
+   def position_taken?(location)
+     @board[location] != " " && @board[location] != "" && @board[location] != nil
+   end
+   
+   def valid_move?
+     index.between?(0,8) && !position_taken?(index)
+   end
+   
+   def turn_count
+     @board.count{|el| el != " " && el != "" && el != nil}
+   end
+   
+   def current_player
+     if turn_count % 2 == 0 
+       "X"
+     else
+       "0"
+     end
+   end
+   
+   def turn
+     puts "Please enter 1-9:"
+     input = gets.strip
+     index = input_to_index(input)
+     if valid_move?(index)
+       
+       player = current_player
+       move(index, plyer)
+       display_board
+     elseturn
+   end
+ end
+ 
+ def won?
+   WIN_COMBINATIONS.each do |el|
+     win_postion_1 = el[0]
+     win_postion_2 = el[1]
+     win_postion_3 = el[2]
+     
+     if @board[win_postion_1] == @board[win_postion_2] && @board 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
